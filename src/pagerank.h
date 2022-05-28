83c2414fd30ae7f30d4cca7c10c7a44dbaf9e113
Sandro Baccega <mail.sandro.baccega@gmail.com>
2022-05-28 11:33:52 +0200

    🎨 Improve PageRank code style

--- src/pagerank.h
+++ src/pagerank.h
@@ -8,6 +8,7 @@
 using namespace std;
 
 float DAMPING_FACTOR = 0.85;
+int LIMIT_ITERATION = 1000;
 
 void stochastization(CSR& csr) {
     vector<int> outDegrees = vector<int>(csr.n_nodes, 0);
@@ -28,7 +29,7 @@ void stochastization(CSR& csr) {
 vector<pair<double, int>> getPageRankTopK(string filename, int topK) {
     CSR csr = CSR(filename);
     int iterations = 0;
-    bool loop = false;
+    bool loop;
     MaxHeap<pair<double, int>> scores(topK);
 
     // Normalize, making the matrix stochastic
@@ -36,41 +37,52 @@ vector<pair<double, int>> getPageRankTopK(string filename, int topK) {
     stochastization(csr);
 
     // PageRank algorithm
-    vector<double> probabilityDistribution = vector<double>(csr.n_nodes, csr.n_nodes);
-    do {
-        loop = false;
-        int rowElement = 0;
-        int currentCol = 0;
-        vector<double> newProbabilityDistribution = vector<double>(csr.n_nodes, 0.0);
+    cout << "\t- PAGERANK" << endl;
 
+    // Initial probability distribution
+    vector<double> p = vector<double>(csr.n_nodes, 1.0 / csr.n_nodes);
+    do {
+        loop = true;
+        int currentRow = 0;
+        int currentIndex = 0;
+
+        vector<double> p_new = vector<double>(csr.n_nodes, 0.0);
+        // Populate the new probability distribution (p_new = p)
         for (int i = 0; i < csr.n_nodes; i++) {
             bool isLastElement = (i == (csr.n_nodes - 1));
-            rowElement = isLastElement ? csr.n_edges - csr.row[i] : csr.row[i + 1] - csr.row[i];
-            for (int j = 0; j < rowElement; j++) {
-                newProbabilityDistribution[csr.index[currentCol]] += csr.values[currentCol] * probabilityDistribution[i];
-                currentCol++;
+            currentRow = isLastElement ? csr.n_edges - csr.row[i] : csr.row[i + 1] - csr.row[i];
+            for (int j = 0; j < currentRow; j++) {
+                p_new[csr.index[currentIndex]] += csr.values[currentIndex] * p[i];
+                currentIndex++;
             }
         }
+        // (p * M)
         for (int i = 0; i < csr.n_nodes; i++) {
-            newProbabilityDistribution[i] = (DAMPING_FACTOR * newProbabilityDistribution[i] + (1.0 - DAMPING_FACTOR)) / csr.n_nodes;
+            p_new[i] = DAMPING_FACTOR * p_new[i] + ((1.0 - DAMPING_FACTOR) / csr.n_nodes);
         }
 
-        if (checkTermination(probabilityDistribution, newProbabilityDistribution)) {
-            loop = true;
+        // Termination conditions
+        if (checkTermination(p, p_new)) {
+            cout << "\t- Converged after " << iterations << " iterations" << endl;
+            loop = false;
+        } else if (iterations > LIMIT_ITERATION) {
+            cout << "\t- Forced termination at " << iterations << " iterations" << endl;
+            loop = false;
         }
 
+        // Update p for next iteration
         for (int i = 0; i < csr.n_nodes; i++) {
-            probabilityDistribution[i] = newProbabilityDistribution[i];
+            p[i] = p_new[i];
         }
 
         iterations++;
     } while (loop);
 
-    cout << "\t- Converged after " << iterations << " iterations" << endl;
-
     // Find top K nodes using a MaxHeap
+    cout << "\t- SORTING RESULTS" << endl;
     for (int i = 0; i < csr.n_nodes; i++) {
-        auto pair = make_pair(probabilityDistribution[i], i + 1);
+        auto pair = make_pair(p[i], i + 1);
+        cout << "\t- PAIR MADE " << i << ": " << pair.first << endl;
         scores.push(pair);
     }
 
